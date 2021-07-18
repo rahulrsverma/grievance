bdf927e7048a9b4a9f8ab3225da39b56135e1b81
rahulrsverma <rahulverma.22394@gmail.com>
2021-07-18 13:01:50 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,68 @@
+//import logo from './logo.svg';
+import './App.css';
+import React from 'react'
+import Counter from './components/Counter';
+
+
+function App() {
+
+
+return ( <Counter/> )
+
+
+
+  // return (
+  //   <h1>Grievance Form<br></br><h7>Email</h7><br></br><h7>Title</h7><br></br><h7>Description</h7><br></br><h7>Submit</h7></h1>
+   
+
+// const useers = {
+// name:'Sai',
+// prof:'S/W'
+// },
+// {
+//   name:'Charan',
+//   prof:'Teaching'
+// }
+// ]
+
+
+
+// return (
+//   <ul>
+//  {useers.map((user,index)=> {
+
+
+//  })}
+
+//   </ul>
+
+// )
+
+
+  // <div>
+  //   <FunctionalComponent name="Sai" prof="S/W"/>
+
+  //   <ClassComponent name="Charan" prof="S/W" />
+  //   </div>
+
+
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Keep <code>working and  working </code> and keep Practising..
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+  //   // </div>
+  // );
+ }
+
+export default App;
